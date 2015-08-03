63b562db638fb258c18d75d45a3202993f9eed93
evcaldwell <evcaldwell@gmail.com>
2015-08-03 15:04:01 -0400

    added documentation to JS file

--- js/main.js
+++ js/main.js
@@ -7,25 +7,32 @@
 //
 // Allow users to click the images to see a larger version with more information.
 $(document).on('ready', function(){
-
+//this
 	var searchImages = function(tags){
 		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 		console.log (tags);
+		//I included a colsole.log here to make sure that the user input was loading correctly
 		$.getJSON( flickrAPI, {
+			//Iquery example about Mt. Rainier but with tags switched to be open to user input
    			 tags: tags,
    			 tagmode: "any",
     		format: "json"
+			//the done function in jQuery is for when the json  call is successful
 		}).done(function( data ) {
+			//.empty clears out the page, so a new search won't load images after the previous images
 			$("#images").empty();
     		$.each( data.items, function( i, item ) {
+				//this creates a new <li> for each new image
 				var newItem = $("<li>");
 				newItem.appendTo("#images");
         		$( "<img>" ).attr( "src", item.media.m ).appendTo( newItem );
+				//this changes the html to reflect the search input of the user
 				$('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
-    
+    //this adds the title/date/link to each image
        			var newTitle = $('<p class="image-title">').html(item.title).appendTo(newItem);
        			var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newItem);
         		var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newItem);
+				// this is my limit on the number of images I'd like loaded onto the page
         		if ( i === 15 ) {
         			return false;
         		}
